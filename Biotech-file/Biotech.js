bfe533a9ae614d8e727533a563fdc9d1e8e117af
Informatico17 <fabriziobianco2022@libero.it>
2025-05-23 23:59:57 +0200

    new modify on .css and .js about new web  page

--- Biotech-file/Biotech.js
+++ Biotech-file/Biotech.js
@@ -41,7 +41,9 @@ function UnHighlight(menu,item) {
    obj=document.getElementById(item);
    obj.style.backgroundColor="CadetBlue";
 }
-   // Clock
+//End drop-down menu
+
+// Clock
 function Clock() {
 if (!document.getElementById) return;
 theclock=document.getElementById("clock");
@@ -54,3 +56,39 @@ if (mins < 10) mins= "0" + mins;
 theclock.innerHTML = hours + ":" + mins + ":" + secs;
 window.setTimeout("Clock();",250);
 }
+//End  Clock
+
+// Lightbox Cellula
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+// End Lightbox Cellula
