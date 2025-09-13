10948425e4275105d5965e05bb01f948ec93621d
Informatico17 <fabriziobianco2022@libero.it>
2025-09-13 19:14:09 +0200

    🔧 Rimosso ModifyInfo.js

--- Biotech-file/ModifyInfo.js
+++
@@ -1,9 +0,0 @@
-// Display last modified date
-document.addEventListener('DOMContentLoaded', function () {
-    const el = document.getElementById('lastModified');
-    if (el) {
-      const d = new Date(document.lastModified);
-      el.textContent = `Last edit: ${d.toLocaleDateString()} ${d.toLocaleTimeString()}`;
-    }
-  });
-  // End display last modified date
